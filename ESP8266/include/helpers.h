92d6c87dda67726a9d88fcac50e80c40fc600e1a
Artur <git@artursterz.de>
2019-02-12 11:26:50 +0100

    Add ESP related code

---
+++ ESP8266/include/helpers.h
@@ -0,0 +1,14 @@
+#ifndef HELPERS_H
+#define HELPERS_H
+
+#define CRISIS_CMD "CRISIS"
+
+// This is the function for writing register. See "write_registers.S" for info.
+extern "C" void write_register_asm(uint32_t a, uint32_t b);
+#define write_register(reg, hostid, par, val) write_register_asm((hostid << 2) + 0x60000a00 + 0x300, (reg | (par << 8) | (val << 16) | 0x01000000))
+
+// MAC addresses used.
+const uint8_t receiver_id[]{0xDE, 0x4F, 0x22, 0x18, 0x2F, 0xB0};
+const uint8_t sender_id[]{0x6A, 0xC6, 0x3A, 0xA5, 0x5F, 0x0E};
+
+#endif
---
+++ ESP8266/src/main.cpp
@@ -0,0 +1,102 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+
+#include <CmdBuffer.hpp>
+#include <CmdCallback.hpp>
+#include <CmdParser.hpp>
+
+#include <WifiEspNow.h>
+
+#include "helpers.h"
+
+// Helpers for parsing commands received via serial.
+CmdParser cmd_parser;
+CmdBuffer<WIFIESPNOW_MAXMSGLEN> cmd_buffer;
+
+// Switch to crisis mode by downclocking the ESP..
+void morph_crisis() {
+    // Notify about switch and wait 100 ms for flush.
+    Serial.print("Received CRISIS command. Morphing to crisis mode.\n");
+    delay(100);
+
+    // This is the actual downclocking.
+    // We have to wait 100 ms until the ESP is back functional
+    write_register(103, 4, 1, 0x48);
+    write_register(103, 4, 2, 0xf1);
+    delay(100);
+
+    // Since we downclocked everything, the baud rate also changes.
+    // The serial monitor, however, still wants 115200, so we have to
+    // speed up the ESP baud rate by 115200 / 13.5 * 32
+    // Again, wait 100 ms for the ESP to be back functional.
+    Serial.begin(273067);
+    delay(100);
+}
+
+// This prints the received data on the receiver.
+// If "crisis" is received, morph to crisis mode.
+void print_log(const uint8_t mac[6], const uint8_t *buf, size_t count, void *cbarg) {
+    if (strcasecmp(CRISIS_CMD, (const char *)buf) == 0) {
+        morph_crisis();
+    } else {
+        Serial.printf("%s\n", (const char *)buf);
+    }
+}
+
+void setup() {
+    Serial.begin(115200);
+    delay(100);
+
+    Serial.print("Setting up Wi-Fi AP.\n");
+
+    // Setting up Wi-Fi AP.
+    WiFi.persistent(false);
+    WiFi.mode(WIFI_AP);
+    WiFi.softAP("emergenCITY", nullptr, 6);
+    WiFi.softAPdisconnect(false);
+
+    // Start the Wi-Fi. If it does not work, restart the device.
+    bool ok = WifiEspNow.begin();
+    if (!ok) {
+        Serial.print("WifiEspNow.begin() failed. Rebooting.\n");
+        ESP.restart();
+    }
+
+    // This is the callback for printing received data (Receiver only).
+    WifiEspNow.onReceive(print_log, nullptr);
+
+    // To be able to communicate, the two ESPs have add each other to the peers list.
+    // Therefore, we first get the mac address, and check which of the two ESPs we have.
+    uint8_t my_mac[6];
+    wifi_get_macaddr(SOFTAP_IF, my_mac);
+
+    ok = WifiEspNow.addPeer((memcmp(sender_id, my_mac, 6) == 0) ? receiver_id : sender_id);
+    if (!ok) {
+        Serial.print("WifiEspNow.addPeer() failed. Rebooting.\n");
+        ESP.restart();
+    }
+}
+
+void loop() {
+    // Timout after 3 seconds.
+    if (!cmd_buffer.readFromSerial(&Serial, 3000)) {
+        return;
+    }
+
+    // Received an error, most likely because empty string.
+    if (cmd_parser.parseCmd(&cmd_buffer) == CMDPARSER_ERROR) {
+        Serial.print("Got empty string.\n");
+        return;
+    }
+
+    // Send the data received over Serial via Wi-Fi.
+    WifiEspNow.send(receiver_id, cmd_buffer.getBuffer(), WIFIESPNOW_MAXMSGLEN);
+
+    // If we have received the "crisis" command, switch to crisis mode.
+    if (cmd_parser.equalCommand(CRISIS_CMD)) {
+        morph_crisis();
+    }
+
+    // Clear the buffer to get rid of remaining junk.
+    cmd_buffer.clear();
+}
